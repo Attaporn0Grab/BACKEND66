8685c60ff47ce1875ffae01645527ea08ba2daa9
Attaporn0Grab <attaporn0samma@gmail.com>
2024-12-11 10:47:58 +0700

    first commit

--- calculator.js
+++ calculator.js
@@ -48,12 +48,4 @@ app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
 
-// app.post("/", (req, res) => {
-//     console.log(req.body);
-//     console.log(req.body.num1);
-//     console.log(req.body.num2);
-//     var result = Number(req.body.num1) +  Number
-//     (req.body.num2)
-//     res.send("นำเลข 2 ตัวมาบวกกันได้เท่ากับ :  = " + result);
 
-// });
